11b1a916388ecb62d6195c8282246da8ec147942
Michael Shea <michael.shea@imc.com>
2022-08-24 21:00:11 -0500

    add cors headers for cookie issue

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'landing.apps.LandingConfig',
-    'wagtail'
+    'wagtail',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
